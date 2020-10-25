381062ecbbcf1099119848ce49f05abdfdfb40a5
Artur Kardas <kardasartur1@gmail.com>
2020-10-25 14:11:34 +0100

    aktualizacja

--- FootballMenago/TeamViewController.swift
+++ FootballMenago/TeamViewController.swift
@@ -11,8 +11,7 @@ class TeamViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
 }
