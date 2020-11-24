093ec26f0fa4086541ae82ebbd089c7ad31fdfa3
Artur Kardas <kardasartur1@gmail.com>
2020-11-24 01:53:35 +0100

    odczytywanie i zapisywanie do pliku zawodnicy.json

---
+++ FootballMenago/MODELS/zawodnicyMODEL.swift
@@ -0,0 +1,18 @@
+//
+//  zawodnicyMODEL.swift
+//  FootballMenago
+//
+//  Created by Artur on 18/11/2020.
+//
+
+import Foundation
+
+struct Teams: Codable{
+    var teams: [Team]
+}
+
+struct Team: Codable{
+    let title: String
+    var names: [String]
+    var numbers: [Int]
+}
--- FootballMenago/MatchViewController.swift
+++ FootballMenago/MatchViewController.swift
@@ -12,7 +12,7 @@ class MatchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print("Meczyk")
     }
 
 }
